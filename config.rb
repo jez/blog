0999336686e0fee6b59aa1e41c2481cc79b408d2
Jacob Zimmerman <zimmerman.jake@gmail.com>
2014-05-06 00:40:19 -0400

    New themes

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/github/"
-http_images_path = "/github/images"
+http_path = "/"
+http_images_path = "/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/github/fonts"
-css_dir = "public/github/stylesheets"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
