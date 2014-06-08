6cd1766e7f7f0eca942b6f04cfef46e436175896
Jacob Zimmerman <zimmerman.jake@gmail.com>
2014-06-08 04:14:41 -0400

    Wrap all images from {% img %} in link
    Uses the src attribute passed as an argument to the plugin to set the
    href attribute on the anchor tag.

--- plugins/image_tag.rb
+++ plugins/image_tag.rb
@@ -39,7 +39,7 @@ module Jekyll
 
     def render(context)
       if @img
-        "<img #{@img.collect {|k,v| "#{k}=\"#{v}\"" if v}.join(" ")}>"
+        "<a href=\"#{@img['src']}\"><img #{@img.collect {|k,v| "#{k}=\"#{v}\"" if v}.join(" ")}></a>"
       else
         "Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \"title text\" [\"alt text\"]] %}"
       end
