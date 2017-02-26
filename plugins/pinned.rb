2f6f1cec3597632b62756e195fab50ab2407cc15
Jacob Zimmerman <zimmerman.jake@gmail.com>
2017-02-25 23:46:42 -0500

    Add plugin for doing pinned posts in Jekyll 2.x

--- plugins/pinned.rb
+++ plugins/pinned.rb
@@ -1,16 +1,37 @@
-Jekyll::Hooks.register :site, :pre_render do |site, payload|
-  # Sort pinned posts up!
-  # https://talk.jekyllrb.com/t/pinned-posts-like-wordpress/1595/4
+# This "should" work on Jekyll 3.x, but we're using Jekyll 2.x
+#Jekyll::Hooks.register :site, :pre_render do |site, payload|
+#  # Sort pinned posts up!
+#  # https://talk.jekyllrb.com/t/pinned-posts-like-wordpress/1595/4
+#
+#  pinned_posts = payload['site']['posts'].select { |p| p.data['pinned'] }
+#  pinned_posts.sort_by!{ |p|
+#    pinned = p.data['pinned']
+#    if pinned == true
+#      pinned = 1
+#    elsif pinned == false
+#      pinned = 0
+#    end
+#    [pinned, p.date]
+#  }.reverse!
+#  payload['site']['pinned'] = pinned_posts
+#end
 
-  pinned_posts = payload['site']['posts'].select { |p| p.data['pinned'] }
-  pinned_posts.sort_by!{ |p|
-    pinned = p.data['pinned']
-    if pinned == true
-      pinned = 1
-    elsif pinned == false
-      pinned = 0
-    end
-    [pinned, p.date]
-  }.reverse!
-  payload['site']['pinned'] = pinned_posts
+class PinnedPosts < Octopress::Hooks::Site
+  def merge_payload(payload, site)
+    # Sort pinned posts up!
+    # https://talk.jekyllrb.com/t/pinned-posts-like-wordpress/1595/4
+
+    pinned_posts = payload['site']['posts'].select { |p| p.data['pinned'] }
+    pinned_posts.sort_by!{ |p|
+      pinned = p.data['pinned']
+      if pinned == true
+        pinned = 1
+      elsif pinned == false
+        pinned = 0
+      end
+      [pinned, p.date]
+    }.reverse!
+    payload['site']['pinned'] = pinned_posts
+    {}
+  end
 end
